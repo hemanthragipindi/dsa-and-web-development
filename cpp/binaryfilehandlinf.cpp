1c8df6ad4f80b32bcccfb4977ec64ddebc200d90
RAGIPINDI HEMANTH REDDY <ragipindihemanth713@gmail.com>
2026-09-22 13:10:06 +0530

    Add binary file handling example and create student data file

---
+++ cpp/binaryfilehandlinf.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+class student{
+    public:
+
+    int roll ;
+    char name[20];
+    float marks;
+
+};
+int main(){
+    student s;
+    cout<<"enter the roll number :- ";
+    cin>>s.roll;
+    cout<<"enter the name :- ";
+    cin>>s.name;
+    cout<<"enter the marks :- ";
+    cin>>s.marks;
+    ofstream file("student.dat",ios::binary);
+    file.write((char*)&s,sizeof(s));
+    file.close();
+    return 0;
+}
